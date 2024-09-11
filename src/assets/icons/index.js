17012735907bd92ba98722108b495eb8516a7bee
Sudarshan Adhikari <sdadkry95@gmail.com>
2024-09-11 13:41:14 +0545

    search and menu header

--- src/assets/icons/index.js
+++ src/assets/icons/index.js
@@ -3,5 +3,6 @@ import HeartIcon from "./Heart.svg";
 import ArrowDownIcon from "./ArrowDown.svg";
 import CartIcon from "./Cart.svg";
 import SearchIcon from "./Search.svg";
+import PhoneIcon from "./Phone.svg";
 
-export { Location, HeartIcon, ArrowDownIcon, CartIcon, SearchIcon };
+export { Location, HeartIcon, ArrowDownIcon, CartIcon, SearchIcon, PhoneIcon };
