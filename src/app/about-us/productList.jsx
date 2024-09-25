d9cdeda2b70e825b95b56d07cabce242515cfef5
Sudarshan Adhikari <sdadkry95@gmail.com>
2024-09-25 14:17:49 +0545

    feat: search product

--- src/app/about-us/productList.jsx
+++ src/app/about-us/productList.jsx
@@ -4,31 +4,60 @@ import Image from "next/image";
 import React, { useEffect, useState } from "react";
 
 const ProductList = () => {
-  const [product, setProduct] = useState([]);
+  const [searchText, setSearchText] = useState("");
+
+  const [products, setProducts] = useState([]);
+
+  const [filteredProds, setFilteredProds]  = useState([]);
 
   const fetchProduct = async () => {
     const res = await fetch("https://fakestoreapi.com/products");
 
     const json = await res.json();
 
-    setProduct(json ?? []);
+    setProducts(json ?? []);
+    setFilteredProds(json ?? []);
   };
 
   useEffect(() => {
     fetchProduct();
   }, []);
 
-  console.log(product);
+  const handleSearch = () => {
+
+    const filteredProds = products.filter((val) => {
+      return val.title.toLowerCase().includes(searchText.toLowerCase())
+    })
+
+    setFilteredProds(filteredProds)
+
+  }
 
   return (
-    <div className="grid grid-cols-5 gap-16">
-      {product.map((prod) => (
-        <div>
-          <Image width={500} height={500} src={prod.image} className="w-full h-[400px]" />
-          <p>{prod.title}</p>
-        </div>
-      ))}
-    </div>
+    <>
+      <div className="flex items-center">
+        <input
+          type="text"
+          className="border-2 border-slate-500"
+          value={searchText}
+          onChange={(e) => setSearchText(e.target.value)}
+        />
+        <button onClick={handleSearch}>Search</button>
+      </div>
+      <div className="grid grid-cols-5 gap-16">
+        {filteredProds.map((prod) => (
+          <div>
+            <Image
+              width={500}
+              height={500}
+              src={prod.image}
+              className="w-full h-[400px]"
+            />
+            <p>{prod.title}</p>
+          </div>
+        ))}
+      </div>
+    </>
   );
 };
 
